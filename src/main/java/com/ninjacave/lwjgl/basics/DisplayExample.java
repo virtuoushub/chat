c60a285cce214bc564b354e154e1b0fdb65767c2
Peter Colapietro <petercolapietro@gmail.com>
2014-10-23 05:27:31 -0500

    Formatting/Whitespace.

--- src/main/java/com/ninjacave/lwjgl/basics/DisplayExample.java
+++ src/main/java/com/ninjacave/lwjgl/basics/DisplayExample.java
@@ -6,34 +6,29 @@ import org.lwjgl.opengl.DisplayMode;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-public class DisplayExample {
-
-    final static Logger log = LoggerFactory.getLogger(DisplayExample.class);
  
+public class DisplayExample {
+    
+    final static Logger log = LoggerFactory.getLogger(DisplayExample.class);
+
     public void start() {
         try {
-        Display.setDisplayMode(new DisplayMode(800,600));
-        Display.create();
-    } catch (LWJGLException e) {
-        log.error("Failed to create display. {}",e);
-        System.exit(0);
-    }
- 
-    // init OpenGL here
- 
-    while (!Display.isCloseRequested()) {
- 
-        // render OpenGL here
- 
-        Display.update();
-    }
- 
-    Display.destroy();
+            Display.setDisplayMode(new DisplayMode(800,600));
+            Display.create();
+        } catch (LWJGLException e) {
+            log.error("Failed to create display. {}",e);
+            System.exit(0);
+        }
+        // init OpenGL here
+        while (!Display.isCloseRequested()) {
+            // render OpenGL here
+            Display.update();
+        }
+        Display.destroy();
     }
  
     public static void main(String[] argv) {
         DisplayExample displayExample = new DisplayExample();
-    displayExample.start();
+        displayExample.start();
     }
 }
